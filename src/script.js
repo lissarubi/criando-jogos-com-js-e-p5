87f66d2cdda76fd0128b3fc9b91312628ac465d3
Lissa Ferreira <lissadeveloper@gmail.com>
2021-01-02 02:22:17 -0400

    adicionando circle.png

--- src/script.js
+++ src/script.js
@@ -2,28 +2,32 @@ var square
 
 function setup(){
   createCanvas(700, 700)
-  square = createSprite(450, 450)
+  square = createSprite(350, 350)
   squareImg = loadImage('https://raw.githubusercontent.com/edersonferreira/criando-jogos-com-js-e-p5/main/src/assets/square.png')
   square.addImage(squareImg)
+
+  setInterval(createEnemy, 3000)
+}
+
+function createEnemy(){
+
 }
 
 function draw(){
   background(230)
 
-
-
-//   if (keyDown('W')){
-//     square.position.y -= 5
-//   }
-//   if (keyDown('S')){
-//     square.position.y += 5
-//   }
-//   if (keyDown('A')){
-//     square.position.x -= 5
-//   }
-//   if (keyDown('D')){
-//     square.position.x += 5
-//   }
+  if (keyDown('W')){
+    square.position.y -= 5
+  }
+  if (keyDown('S')){
+    square.position.y += 5
+  }
+  if (keyDown('A')){
+    square.position.x -= 5
+  }
+  if (keyDown('D')){
+    square.position.x += 5
+  }
 
   drawSprites()
 }
